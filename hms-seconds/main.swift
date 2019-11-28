d268e3a5d722a53652a9504214aebf10173edbca
Gene De Lisa <gene@rockhoppertech.com>
2019-11-28 11:31:57 -0500

    organization

--- hms-seconds/main.swift
+++ hms-seconds/main.swift
@@ -8,50 +8,9 @@
 
 import Foundation
 
-struct HMS2Seconds {
-    var hms: String
-    var isVerbose = false
-    
-    func hmsToSeconds() -> Int {
-        
-        if isVerbose {
-            print("parsing hms input: \(hms)")
-        }
-        
-        let componentArray = hms.split(separator: ":")
-        if componentArray.count != 3 {
-            print("Error: \(hms) is invalid. Must have 3 components: hours, minutes, and seconds.")
-            exit(EXIT_FAILURE)
-        }
-        guard let h = Int(componentArray[0]),
-            let m = Int(componentArray[1]),
-            let s = Int(componentArray[2]) else {
-                print("Error \(hms) is invalid")
-                exit(EXIT_FAILURE)
-        }
-        
-        if isVerbose {
-            print("h \(h) m \(m) s \(s)")
-        }
-        
-        let ms = m * 60
-        let hs = h * 60 * 60
-        let totalSeconds = s + ms  + hs
-        return totalSeconds
-    }
-}
 
 let ap = TSCArgumentParser()
 ap.crack()
 
 exit(EXIT_SUCCESS)
 
-
-/*
- if CommandLine.argc > 1 {
- let hms = CommandLine.arguments[1]
- } else {
- print("specify input. hh:mm:ss")
- exit(EXIT_FAILURE)
- }
- */
